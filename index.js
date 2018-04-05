7ceb9ae260c10efe2f608692b064a987a8057613
Remi Michel <remi.michel-externe@bio-logic.net>
2018-04-05 10:58:09 +0200

    feat(): Generate CSS with classes

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const {promisify} = require('util');
 const glob = require('glob');
 const svg2ttf = require('svg2ttf');
 const SVGIcons2SVGFontStream = require('svgicons2svgfont');
+const css = require('node-css');
+const randomUnicode = require('random-unicodes');
 
-const fontStream = new SVGIcons2SVGFontStream({
-  fontName: 'neytiri'
-});
+const fontName = 'neytiri';
 const svgFontFile = 'fonts/neytiri.svg';
 const ttfFontFile = 'fonts/neytiri.ttf';
 const iconsPath = 'icons';
+
+const writeFile = promisify(fs.writeFile);
+const readFile = promisify(fs.readFile);
+const fontStream = new SVGIcons2SVGFontStream({fontName});
 
 // Setting the font destination
 fontStream.pipe(fs.createWriteStream(svgFontFile))
@@ -23,15 +27,28 @@ fontStream.pipe(fs.createWriteStream(svgFontFile))
 const createGlyph = () => {
   const pGlob = promisify(glob);
   return pGlob(`${iconsPath}/*.svg`, {}).then(files => {
-    return files.map(file => {
+    const hexCodes = generateHexCodes(files.length);
+    return files.map((file, idx) => {
       const glyph = fs.createReadStream(file);
+      const name = getGlyphName(file);
       glyph.metadata = {
-        unicode: ['\uE001'],
-        name: getGlyphName(file)
+        unicode: [JSON.parse('"' + hexCodes[idx] + '"')],
+        name
       };
-      return glyph;
+      return {glyph, name, code: hexCodes[idx]};
     });
   });
+};
+
+const generateHexCodes = (nbCodes, acc = []) => {
+  if(nbCodes === 0){
+    return acc;
+  }
+  const hexCode = randomUnicode({ max: '\\uFFFF' });
+  if(acc.includes(hexCode)){
+    return generateHexCodes(nbCodes, acc)
+  }
+  return generateHexCodes(nbCodes - 1, acc.concat(hexCode))
 };
 
 
@@ -40,16 +57,32 @@ const getGlyphName = (fileName) => {
 };
 
 const generateFontFile = (fileIn, fileOut) => {
-  const writeFile = promisify(fs.writeFile);
-  const readFile = promisify(fs.readFile);
-  return readFile(fileIn, 'utf8')
-    .then(fontSVG => writeFile(fileOut, new Buffer(svg2ttf(fontSVG, {}).buffer)))
+  return readFile(fileIn, 'utf8').then(fontSVG => writeFile(fileOut, new Buffer(svg2ttf(fontSVG, {}).buffer)))
+};
+
+const generateCSSWithClasses = (glyphsNameAndCode) => {
+  let cssClasses = css('@font-face', {
+    'font-family': `'${fontName}'`,
+    src: `url('${ttfFontFile}') format('truetype'), url('${svgFontFile}') format('svg')`,
+    'font-weight': 'normal',
+    'font-style': 'normal'
+  });
+  cssClasses += css(`[class^="${fontName}-"], [class*="${fontName}-"]`, { 'font-family': `${fontName} !important` });
+  cssClasses += glyphsNameAndCode.map(({ name, code }) => {
+    return css(`.${fontName}-${name}:before`, { content: `'\\${code.replace('\\u', '')}'` })
+  }).join('\n');
+  return cssClasses;
 };
 
 
 (function () {
   createGlyph()
-    .then(streams => streams.forEach(stream => fontStream.write(stream)))
-    .then(() => fontStream.end())
+    .then((glyphs) => {
+      glyphs.forEach(glyph => fontStream.write(glyph.glyph))
+      fontStream.end();
+      return glyphs.map(glyph => ({ name: glyph.name, code: glyph.code }));
+    })
+    .then((glyphsNameAndCode) => generateCSSWithClasses(glyphsNameAndCode))
+    .then((css) => writeFile('style.css', css))
     .catch(err => console.log(err))
 })();
